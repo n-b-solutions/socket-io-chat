b0be02f7971ce2006620e9a3036abcd64fdfc9ed
Sara Cohen <sc0583218777@n-b-solutions.com>
2022-11-13 14:48:01 +0200

    change the port to 3333

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const socketio = require("socket.io");
 const app = express();
 app.use(express.static(__dirname + "/client"));
 
-const expressServer = app.listen(3000, () => {
-  console.log("listening on http://localhost:3000");
+const expressServer = app.listen(3333, () => {
+  console.log("listening on http://localhost:3333");
 });
 
 const io = socketio(expressServer);
